bcf6607005177326bd88447b1ba9088743333932
Yoni <yonatansamfisher@gmail.com>
2021-08-04 19:56:21 +0300

    updated

---
+++ main.js
@@ -0,0 +1,11 @@
+var firstName = prompt("What is your name")
+
+var numberOne = +prompt("Type your numbers")
+var numberTwo = +prompt("Type your numbers")
+var result = numberOne + numberTwo
+console.log(result)
+// var a = 5;
+// var b = 8;
+
+// var c = a + b
+
